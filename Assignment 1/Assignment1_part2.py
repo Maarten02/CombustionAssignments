24e2c4c6d083a9a40d1b73788879a528b37ccc41
Maarten02 <m.e.krikke@student.tudelft.nl>
2023-05-10 14:56:39 +0200

    restructure the shit some more

--- Assignment 1/Assignment1_part2.py
+++ Assignment 1/Assignment1_part2.py
@@ -1,66 +1,39 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 # Open the file and read the lines
-with open('./../../cea-exec/C2H4.plt', 'r') as f:
-    lines = f.readlines()
+
+# Get path to file, run and plot
+h2_filenames = ['c2h4_p', 'c2h4_phi', 'h2_p', 'h2_phi']
+x_vars = ['p', 'phi']
+y_vars = ['']
+folder = './CEA_plt/'
+extension = '.plt'
+
+def readFile(path, x_var, y_var):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        # Skip the comments and empty lines
+        if line.startswith('#'):
+            col_names = [str(x) for x in line.split()[1:]]
+            continue
+        # Split the line into columns
+
+        cols = line.split()
+
+        # Convert the columns to floats and store the data
+        row = [float(x) for x in cols]
+
+        if not 'data' in locals():
+            data = np.array(row)
+        else:
+            data = np.vstack((data, row))
 
 
-
-# Iterate over the lines
-for line in lines:
-    # Skip the comments and empty lines
-    if line.startswith('#'):
-        col_names = [str(x) for x in line.split()[1:]]
-        continue
-    # Split the line into columns
-
-    cols = line.split()
-
-
-
-    # Convert the columns to floats and store the data
-    row = [float(x) for x in cols]
-
-    if not 'data' in locals():
-        data = np.array(row)
-    else:
-        data = np.vstack((data, row))
-
-# Print the column names and the data
-print(col_names)
-print(data)
-#
-#
-# import numpy as np
-#
-# # Open the file and read the lines
-# with open('./../../cea-exec/C2H4.plt', 'r') as f:
-#     lines = f.readlines()
-#
-# # Initialize the list for column names
-# col_names = []
-#
-# # Iterate over the lines
-# for line in lines:
-#     # Skip the comments and empty lines
-#     if line.startswith('#') or line.strip() == '':
-#         continue
-#
-#     # Split the line into columns
-#     cols = line.split()
-#
-#     # Store the column names
-#     if not col_names:
-#         col_names = cols
-#         continue
-#
-#     # Convert the columns to floats and store the data
-#     row = [float(x) for x in cols]
-#     if not 'data' in locals():
-#         data = np.array(row)
-#     else:
-#         data = np.vstack((data, row))
-#
-# # Print the column names and the data
-# print(col_names)
-# print(data)
+    # # TODO: plot and save figure
+    # temp_col=col_names.index('t')
+    # eq_ratio_col = col_names.index('phi')
+    #
+    # plt.plot(data[:,eq_ratio_col], data[:,temp_col])
+    # plt.show()
